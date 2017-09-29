9e415f9410022f3af89cd7f361ccd3872058c2c4
Diogo Dias <diogo.dias.info@gmail.com>
2017-09-29 02:48:22 -0300

    implementacao, contacts e about

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,46 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
-import { NavbarComponent } from './navbar/navbar.component';
+
+//meus componentes
+import { AboutComponent } from './about/about.component';
+import { HeaderComponent } from './header/header.component';
+import { ContactComponent } from './contact/contact.component';
+import { ContactsComponent } from './contacts/contacts.component';
+
+//Importação do serviço de contatos
+import { ContactsService } from './contacts/contacts.service';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    NavbarComponent
+    AboutComponent,
+    HeaderComponent,
+    ContactComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: ContactsComponent
+      },
+      {
+        path: 'about',
+        component: AboutComponent
+      },
+    ])
   ],
-  providers: [],
+  providers: [ContactsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
